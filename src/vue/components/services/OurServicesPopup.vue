48f1ad0894eca2c752bb87cb9f54a1956f01f911
gaba-work <malkocmax@gmail.com>
2025-09-11 11:46:28 +0300

    feat: finished our services section todo: button Contacci ora

--- src/vue/components/services/OurServicesPopup.vue
+++ src/vue/components/services/OurServicesPopup.vue
@@ -21,18 +21,20 @@ function emitClose() {
 <template>
   <div class="our-services-popup">
     <div class="our-services-popup__overlay" @click.self="emitClose"></div>
-    <div class="our-services-popup__inner">
-      <header class="our-services-popup__header">
-        <h3 class="our-services-popup__title">{{ title }}</h3>
-        <button
-          @click="emitClose"
-          class="our-services-item__btn our-services-item__btn--red"
-        >
-          <IMinus />
-        </button>
-      </header>
-      <div class="our-services-popup__body">
-        <div class="our-services-popup__description" v-html="text"></div>
+    <div class="our-services-popup__wrapper">
+      <div class="our-services-popup__inner">
+        <header class="our-services-popup__header">
+          <h3 class="our-services-popup__title">{{ title }}</h3>
+          <button
+            @click="emitClose"
+            class="our-services-item__btn our-services-item__btn--red"
+          >
+            <IMinus />
+          </button>
+        </header>
+        <div class="our-services-popup__body">
+          <div class="our-services-popup__description" v-html="text"></div>
+        </div>
       </div>
     </div>
   </div>
